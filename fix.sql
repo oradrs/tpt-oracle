0ac25355f25f18ccde485aafbb84c6fb31a4c3da
oradrs <oradrs@yahoo.com>
2021-12-04 18:41:02 +0530

    added column formatting and sample cmd

--- fix.sql
+++ fix.sql
@@ -1,6 +1,11 @@
 -- Copyright 2018 Tanel Poder. All rights reserved. More info at http://tanelpoder.com
 -- Licensed under the Apache License, Version 2.0. See LICENSE.txt for terms & conditions.
 
+col bugno                           format 999999999
+col VALUE                           format 999
+col sql_feature                     format a40
+col optimizer_feature_enable        format a10 heading 'Opt|features'
+
 SELECT
     *
 FROM
@@ -15,3 +20,6 @@ AND (
     )
 /
 
+prompt 
+prompt *** SAMPLE - to disable at session level : alter session set "_fix_control" = '21183079:0';
+prompt 
