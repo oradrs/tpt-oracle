8b7e1605de34a834d9871383da9e0bda26f1eac5
oradrs <oradrs@yahoo.com>
2022-09-02 21:20:32 +0530

    added parameters 1 & 2

--- itl_genDDL.sql
+++ itl_genDDL.sql
@@ -3,21 +3,44 @@
 -- 29-Sep-2021
 -- ------------------------------------------
 
+DEF schemaName = '&1';
+UNDEF 1;
+DEF tabname = '&2';
+UNDEF 2;
+
+-- ------------------------------------------
+SET head on;
+spool itl_getTabDtl.log append;
+pro -- Table : &&tabname
+
+@itl_getTabDtl.sql 
+
+spool off;
+
+-- ------------------------------------------
+
+SET head off;
+SET TIMING off;
+
+spool itl_genDDL.log append;
+pro -- Table : &&tabname
 
 select 'ALTER TABLE ' || table_name || ' INITRANS 50;' stmt_1
 from user_tables
-where table_name = '&tabname';
+where table_name = upper('&&tabname');
 
 select 'ALTER TABLE ' || table_name || ' MOVE;' stmt_2
 from user_tables
-where table_name = '&tabname';
+where table_name = upper('&&tabname');
 
-select 'ALTER TABLE ' || index_name || ' rebuild INITRANS 50;' stmt_3
+select 'ALTER INDEX ' || index_name || ' rebuild INITRANS 50;' stmt_3
 from user_indexes
-where table_name = '&tabname';
+where table_name = upper('&&tabname')
+ORDER BY index_name;
 
 -- sample to gather stats
--- exec dbms_stats.gather_table_stats(ownname => 'schemaName', tabname => 'tabName', estimate_percent => DBMS_STATS.AUTO_SAMPLE_SIZE, method_opt => 'FOR ALL COLUMNS SIZE AUTO', degree => DBMS_STATS.AUTO_DEGREE, cascade => TRUE);
+Prompt 
+Prompt exec dbms_stats.gather_table_stats(ownname => upper('&&schemaName'), tabname => upper('&&tabname'), estimate_percent => DBMS_STATS.AUTO_SAMPLE_SIZE, method_opt => 'FOR ALL COLUMNS SIZE AUTO', degree => DBMS_STATS.AUTO_DEGREE, cascade => TRUE);;
 
 -- ------------------------------------------
 
@@ -29,3 +52,9 @@ where table_name = '&tabname';
 --    alter index IDX1  rebuild INITRANS 50;
 --
 --    exec dbms_stats.gather_table_stats(ownname => 'SCOTT', tabname => 'EMP', estimate_percent => DBMS_STATS.AUTO_SAMPLE_SIZE, method_opt => 'FOR ALL COLUMNS SIZE AUTO', degree => DBMS_STATS.AUTO_DEGREE, cascade => TRUE);
+
+pro -------------------------------------------;
+spool off;
+
+undefine schemaName;
+undefine tabname;
